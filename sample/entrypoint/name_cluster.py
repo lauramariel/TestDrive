a09a3bc0f3d0c04b46fa58c45825ed4090e2bb5b
Laura Jordana <lauram93@gmail.com>
2020-03-05 15:17:23 -0800

    fix import

--- sample/entrypoint/name_cluster.py
+++ sample/entrypoint/name_cluster.py
@@ -6,33 +6,27 @@ import time
 sys.path.append(os.path.join(os.getcwd(), "nutest_gcp.egg"))
 
 from framework.lib.nulog import INFO, ERROR
-from framework.entities.image.image import Image
-from framework.interfaces.interface import Interface
 from framework.entities.cluster.nos_cluster import NOSCluster
 
 def name_cluster(cluster, name):
     cluster.execute('ncli cluster edit-params new-name="{}"'.format(name))
 
-
-
 def main():
-  config = json.loads(os.environ["CUSTOM_SCRIPT_CONFIG"])
+    config = json.loads(os.environ["CUSTOM_SCRIPT_CONFIG"])
 
-  INFO(config)
+    INFO(config)
 
-  cvm_info = config.get("tdaas_cluster")
+    cvm_info = config.get("tdaas_cluster")
 
-  cvm_external_ip = cvm_info.get("ips")[0][0]
-  cvm_internal_ip = cvm_info.get("ips")[0][1]
+    cvm_external_ip = cvm_info.get("ips")[0][0]
+    # cvm_internal_ip = cvm_info.get("ips")[0][1]
 
-  cluster = NOSCluster(cluster=cvm_external_ip, configured=False)
-  cluster_name = "MyCluster"
-  
-  name_cluster(cluster=cluster, name=cluster_name)
+    cluster = NOSCluster(cluster=cvm_external_ip, configured=False)
+    cluster_name = "MyCluster"
+ 
+    name_cluster(cluster=cluster, name=cluster_name)
 
-  time.sleep(30)
+    time.sleep(30)
 
 if __name__ == '__main__':
-  main()
-
-
+    main()
