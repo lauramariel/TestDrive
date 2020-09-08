64c42170f0bd51e1980c9978a475a52091cf2081
Laura Jordana <laura@nutanix.com>
2020-09-08 09:11:29 -0700

    register FA before applying minerva patch

--- files-phase-2/entrypoint/entrypoint.sh
+++ files-phase-2/entrypoint/entrypoint.sh
@@ -21,9 +21,10 @@ python resolve_alerts.py
 python fa_upload.py
 python fa_prep.py
 python fa_deploy.py
-python fa_patches.py
 sleep 60
 python fa_register.py
+python fa_patches.py
+
 # sleep to ensure that Windows VM startup script is 
 # finished before deployment is marked as complete
 sleep 600
