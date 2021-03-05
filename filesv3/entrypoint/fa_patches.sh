9aeba2ac6424d5c80933b6693c57e672d548c2d3
Laura Jordana <laura@nutanix.com>
2021-03-05 11:54:38 -0800

    update script to include patch for FSM 2.0.1 with support for port 443

--- filesv3/entrypoint/fa_patches.sh
+++ filesv3/entrypoint/fa_patches.sh
@@ -8,19 +8,26 @@
 set -ex
 
 CONFIG="/home/config.json"
-#CONFIG="./config3.json" # delete after testing
+#CONFIG="./config4.json" # delete after testing
 echo $CONFIG
 
 UPDATE_FA_FILE_PATH="https://storage.googleapis.com/testdrive-templates/files/deepdive/fa_update_zk.tar"
-UPDATE_MINERVA_PATH="https://storage.googleapis.com/testdrive-templates/files/deepdive/nutanix-minervacvm-el7.3-release-fsm-1.1.1-stable-e4a1334b74e7b8efa6d5d342bf7cce8e478164b2-x86_64.tar.gz"
-MINERVA_PACKAGE_PATH="/home/nutanix/nutanix-minervacvm-el7.3-release-fsm-1.1.1-stable-e4a1334b74e7b8efa6d5d342bf7cce8e478164b2-x86_64.tar.gz"
+UPDATE_MINERVA_PATH="https://storage.googleapis.com/testdrive-templates/filesv3/deepdive/nutanix-minervacvm-el7.3-release-fsm-2.0.1-stable-4e7c964481d81415110700410eddaea920993df0-x86_64.tar.gz"
+MINERVA_PKG_PATH="/home/nutanix/nutanix-minervacvm-el7.3-release-fsm-2.0.1-stable-4e7c964481d81415110700410eddaea920993df0-x86_64.tar.gz"
 
 # Get PE IP, and strip suffix and prefix "
 PE_IP=$(jq '.tdaas_cluster.ips[0][0]' $CONFIG)
-PE_IP="${PE_IP%\"}" # deletes the " from the end 
+PE_IP="${PE_IP%\"}" # deletes the " from the end
 PE_IP="${PE_IP#\"}" # deletes the " from the beginning
 
 echo $PE_IP
+
+# Get PC internal IP for copying to from CVM
+PC_INT_IP=$(jq '.tdaas_pc.ips[0][1]' $CONFIG)
+PC_INT_IP="${PC_INT_IP%\"}" # deletes the " from the end
+PC_INT_IP="${PC_INT_IP#\"}" # deletes the " from the beginning
+
+echo $PC_INT_IP
 
 # Get AVM URL or IP
 AVM_URL=$(jq .\"files-config\".'custom_config.files_url' $CONFIG) || AVM_URL=$(jq '.proxy_vm.public_uvms'.\"public-uvm-1\"'.external_ip' $CONFIG)
@@ -30,6 +37,8 @@ AVM_URL="${AVM_URL#\"}" # deletes the " from the beginning
 
 echo $AVM_URL
 
+# update ZK with proxy URL
+echo "Updating ZK with proxy URL"
 ssh nutanix@$PE_IP "source /etc/profile
 cd /home/nutanix
 curl -kSOL $UPDATE_FA_FILE_PATH
@@ -38,5 +47,18 @@ mv /tmp/fa_update_zk/* /home/nutanix/bin/
 cd /home/nutanix/bin
 python fa_update_zk_node.py --avm_ip=$AVM_URL"
 
-# Todo
-# port 443 support
+# port 443 support - upgrade patch to FSM 2.0.1
+
+echo "Patching FSM to 2.0.1 for 443 support"
+ssh nutanix@$PE_IP "source /etc/profile
+cd /home/nutanix
+curl -kSOL $UPDATE_MINERVA_PATH
+upgrade_fsm.py --pkg_path=$MINERVA_PKG_PATH"
+
+echo "rsync the Prism updates to PC for 443 support"
+# rsync updates to PC
+ssh nutanix@$PE_IP "source /etc/profile
+rsync -rvz --delete --rsh='/usr/bin/sshpass -p \"nutanix/4u\" ssh -o StrictHostKeyChecking=no -l nutanix' \
+/home/nutanix/prism/webapps/console/ nutanix@$PC_INT_IP:/home/nutanix/prism/webapps/console/el7.3-release-euphrates-5.19-stable-09739c1ac339c1505c9181577bb6a0436a936b3a/console"
+
+
