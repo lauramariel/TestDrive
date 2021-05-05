8745659dd1df9446004f2569c8739e98b20f2a81
Laura Jordana <laura@nutanix.com>
2021-05-05 11:46:02 -0700

    fix whitespace

--- pc_cloud_core/entrypoint/pe_pc_aplos_changes.sh
+++ pc_cloud_core/entrypoint/pe_pc_aplos_changes.sh
@@ -8,7 +8,7 @@ CONFIG="/home/config.json"
 
 # Get PC IP, and strip suffix and prefix "
 PC_IP=$(jq '.tdaas_pc.ips[0][0]' $CONFIG)
-PC_IP="${PC_IP%\"}" # deletes the " from the end 
+PC_IP="${PC_IP%\"}" # deletes the " from the end
 PC_IP="${PC_IP#\"}" # deletes the " from the beginning
 echo $PC_IP
 
@@ -16,7 +16,7 @@ ssh nutanix@$PC_IP 'sed -i 's/PERMANENT_SESSION_LIFETIME=900/PERMANENT_SESSION_L
 
 # Get PE IP, and strip suffix and prefix "
 PE_IP=$(jq '.tdaas_cljuster.ips[0][0]' $CONFIG)
-PE_IP="${PC_IP%\"}" # deletes the " from the end 
+PE_IP="${PC_IP%\"}" # deletes the " from the end
 PE_IP="${PC_IP#\"}" # deletes the " from the beginning
 echo $PC_IP
 
